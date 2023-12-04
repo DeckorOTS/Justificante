fe07e1356ef3141fa7b68bcf23f9c3bfe32c065a
DeckorOTS <hectoravilesd@gmail.com>
2023-12-03 23:52:11 -0800

    Mejora en el autoZoom

--- actualizacion.js
+++ actualizacion.js
@@ -113,7 +113,115 @@ document.getElementById("Motivo2").addEventListener("keyup", (event)=>{
         document.getElementById("form19_1").value = content
 });
 document.getElementById("NSS").addEventListener("focus", (event)=>{
-    let zooming = 'zoom:4; right: 120px; top: 530px;'
+    let zooming = 'zoom:4; transform:translate(-120px, 530px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Nombre").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(-190px, 490px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("CURP").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(-130px, 470px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Delegacion").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(-130px, 450px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Amedico").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(-310px, 530px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("CVEPTAL").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(-310px, 420px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("UMF").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(-100px, 430px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Consultorio").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(-90px, 400px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Turno").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(-330px, 400px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Fecha").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(160px, 240px) scale(0.75);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Servicio").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(340px, 130px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Tipo").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(270px, 260px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Ocacion").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(270px, 220px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Fecha1").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(270px, 180px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Cedula").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(-50px, -10px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Matricula").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(-280px, -10px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Motivo1").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(90px, 30px) scale(0.5);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("nombre").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(310px, -10px);';
+    const zoomIn = event.target.style;
+            zooming += zoomIn
+        document.getElementById("formviewer").style = zooming
+})
+document.getElementById("Motivo2").addEventListener("focus", (event)=>{
+    let zooming = 'zoom:4; transform:translate(90px, 1px) scale(0.5);';
     const zoomIn = event.target.style;
             zooming += zoomIn
         document.getElementById("formviewer").style = zooming
